e4d111eccaeb9b1c9e749b2343346290dba4cac6
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-09-08 03:11:22 +0300

    ++

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function ($) {
-  
   //  mobile-menu
 
   jQuery(".burger").click(function () {
@@ -11,12 +10,10 @@ jQuery(document).ready(function ($) {
 
   // category dropdown menu
   if (window.innerWidth > 425) {
-  
   }
 
   if (window.innerWidth < 425) {
     // переношу мобильный контент
-  
   }
   // accordion
 
@@ -26,5 +23,66 @@ jQuery(document).ready(function ($) {
     jQuery(this).next().slideToggle(500);
     jQuery(this).toggleClass("active");
   });
+  // modal
 
+  function modal() {
+    const openModalButtons = document.querySelectorAll("[data-modal-target]");
+    const closeModalButtons = document.querySelectorAll("[data-close-button]");
+    const overlay = document.getElementById("overlay");
+
+    openModalButtons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        e.preventDefault();
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+      });
+    });
+
+    overlay.addEventListener("click", () => {
+      const modals = document.querySelectorAll(".modal.active");
+      modals.forEach((modal) => {
+        closeModal(modal);
+      });
+    });
+
+    closeModalButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const modal = button.closest(".modal");
+        closeModal(modal);
+      });
+    });
+
+    function openModal(modal) {
+      if (modal == null) return;
+      modal.classList.add("active");
+      overlay.classList.add("active");
+    }
+
+    function closeModal(modal) {
+      if (modal == null) return;
+      modal.classList.remove("active");
+      overlay.classList.remove("active");
+    }
+  }
+  modal();
+
+  jQuery(".first__slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+  // slideUp category
+  jQuery(".heating__systems .head").on("click", function () {
+    jQuery(".heating__systems ul").children().slideToggle("slow");
+  });
+  jQuery(".сooling__systems .head").on("click", function () {
+    jQuery(".сooling__systems ul").children().slideToggle("slow");
+  });
+  jQuery(".equipment .head").on("click", function () {
+    jQuery(".equipment ul").children().slideToggle("slow");
+  });
 });
